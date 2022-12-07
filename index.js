721f9ec053595a02cb78853ebf15416eab12012b
Jamrus Ruttanayukol <kiagenwa@gmail.com>
2022-12-07 14:51:51 +0700

    add data processing for FA and PCBA production data

--- index.js
+++ index.js
@@ -42,12 +42,12 @@ connection.on('connect', function(err) {
   executeStatement(qG.getLatestWeek(), weeks => {
     app.get('/', (_, res) => {
       //res.json(weeks);
-      mainDashboard(weeks[1].weeknum, weeks[0].weeknum, 3, res);
+      mainDashboard(weeks[1].weeknum, weeks[0].weeknum, res, 0);
     });
 
     app.post("/select", bodyParser.urlencoded({extended: false}), (req, res) => {
       //res.json(req.body);   // startWeek and endWeek
-      mainDashboard(req.body.startWeek, req.body.endWeek, 3, res);
+      mainDashboard(req.body.startWeek, req.body.endWeek, res, 0);
     });
     
     app.listen(3000, () => {
@@ -76,30 +76,67 @@ function executeStatement(sqlQuery, callback) {
   connection.execSql(request);
 }
 
-function mainDashboard(startWeek, endWeek, pdtype, res) {
-  executeStatement(qG.defectParetoWeeks(startWeek, endWeek, pdtype), (defectsPareto) => {
-    // take out 10th+ pareto data
-    if (defectsPareto.length > 11) defectsPareto.splice(10, defectsPareto.length - 11)
-    executeStatement(qG.dailyRecordWeeks(startWeek, endWeek, pdtype), (dailyRecords) => {
-      const weeklyFOR = {};
-      const model_qty = {};
-      dailyRecords.forEach(d => {
-        if (weeklyFOR[d.weeknum] === undefined) weeklyFOR[d.weeknum] = [ d.defect_qty, d.PDinput];
-        else {
-          weeklyFOR[d.weeknum][0] += d.defect_qty;
-          weeklyFOR[d.weeknum][1] += d.PDinput;
-        }
-        if (model_qty[d.model_name] === undefined) model_qty[d.model_name] = d.PDoutput;
-        else model_qty[d.model_name] += d.PDoutput;
+// main data process, type = 0 is FA
+function mainDashboard(startWeek, endWeek, res, type) {
+  executeStatement(qG.defectParetoWeeks(startWeek, endWeek), (defectsPareto) => {
+    executeStatement(qG.dailyRecordWeeks(startWeek, endWeek), (dailyRecords) => {
+      const data = digestData(type, {
+        defectsPareto: defectsPareto,
+        dailyRecords: dailyRecords
       });
       //res.json(defectsPareto[10]);
       res.render('index', { 
-        weeklyFOR: weeklyFOR,
-        model_qty: model_qty,
-        defects: defectsPareto,
-        PDdata: dailyRecords,
+        weeklyFOR: data.weeklyFOR,
+        model_qty: data.model_qty,
+        defects: data.defectsPareto,
+        totalDefects: data.totalDefects,
+        PDdata: data.dailyRecords,
         startWeek: startWeek,
         endWeek: endWeek });
       });
     });
+}
+
+function digestData (type, data) {
+  // adjust data filtering on type parameter
+  let dataFilter;
+  if (type == 0) dataFilter = (d) => d.pdtypeID == 3;
+  else dataFilter = (d) => d.pdtypeID != 3;
+
+  const defects = data.defectsPareto.filter(dataFilter);
+  const dailyPD = data.dailyRecords.filter(dataFilter);
+  const totalInput = dailyPD.reduce((ttl, d) => ttl + d.PDinput, 0);
+  const totalDefects = defects.reduce((ttl, d) => ttl + d.quantity, 0);
+  const weeklyFOR = {};
+  const model_qty = {};
+
+  // take out 10th+ pareto data
+  if (defects.length > 10) defects.splice(9, defects.length - 10)
+  defects.push({
+    criteria: 'TOTAL',
+    quantity: totalDefects
+  });
+  // calculate dppm
+  // what if there's no defects? to add a condition later.
+  defects.forEach(d => {
+    d.dppm = Math.round(d.quantity * 1000000 / totalInput);
+  });
+
+  dailyPD.forEach(d => {
+    if (weeklyFOR[d.weeknum] === undefined) weeklyFOR[d.weeknum] = [ d.defect_qty, d.PDinput];
+    else {
+      weeklyFOR[d.weeknum][0] += d.defect_qty;
+      weeklyFOR[d.weeknum][1] += d.PDinput;
+    }
+    if (model_qty[d.model_name] === undefined) model_qty[d.model_name] = d.PDoutput;
+    else model_qty[d.model_name] += d.PDoutput;
+  });
+
+  return {
+    weeklyFOR: weeklyFOR,
+    model_qty: model_qty,
+    defectsPareto: defects,
+    dailyRecords: dailyPD,
+    totalDefects: totalDefects
+  };
 }
